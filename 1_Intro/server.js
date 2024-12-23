fe2459028d9370f7722de1b10182c5804417554d
SivaKumarSK <sk.sivakumar.ak@gmail.com>
2024-12-23 21:08:33 +0530

    Chapter-1
    Intro and installation of nodejs.

---
+++ 1_Intro/server.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+//console.log(global);
+
+
+const os = require("os");   // inbuild
+
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+console.log(__dirname);
+console.log(__filename);
+
+console.log("--------------------------------------");
+
+const path = require("path");
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+console.log(path.parse(__filename));
