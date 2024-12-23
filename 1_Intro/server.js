15b0b98736e18c53faa1600fdc282c1c492fb893
SivaKumarSK <sk.sivakumar.ak@gmail.com>
2024-12-23 21:26:17 +0530

    Chapter-1
    Export and import options.

--- 1_Intro/server.js
+++ 1_Intro/server.js
@@ -17,3 +17,20 @@ console.log(path.dirname(__filename));
 console.log(path.basename(__filename));
 console.log(path.extname(__filename));
 console.log(path.parse(__filename));
+
+console.log("--------------------------------------");
+
+//const math = require("./math");
+//console.log(math.add(2 , 3));
+//console.log(math.sub(2 , 3));
+//console.log(math.mul(2 , 3));
+//console.log(math.div(2 , 3));
+
+//OR
+
+const {add , sub , mul , div} = require("./math");
+
+console.log(add(2 , 3));
+console.log(sub(2 , 3));
+console.log(mul(2 , 3));
+console.log(div(2 , 3));
